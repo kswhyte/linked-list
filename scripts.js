8ae96384a2a66d021199d86242f47faa014d3333
KINAN WHYTE <kinanwhyte@gmail.com>
2016-08-30 17:17:23 -0600

    adds site link with test for url validation

--- scripts.js
+++ scripts.js
@@ -1,26 +1,44 @@
 $('#add-link-button').on('click', function() {
-  var titleInput = $('#title-input').val();
   var urlInput = $('#url-input').val();
-  createSiteLink(titleInput, urlInput);
+  isUrlValid(urlInput);
 });
 
-function createSiteLink(title, url) {
-  var siteLink = '<li><a href="url">title</a><button class="remove-link">x</button><button class="mark-as-read">Mark as Read</li>'
+function isUrlValid(urlInput) {
+  var urlCheck = urlInput.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+  if (urlCheck === null) {
+    invalidUrlErrorMessage();
+  } else {
+    createSiteLink(urlInput);
+  }
+}
+
+function invalidUrlErrorMessage() {
+  alert("That URL is invalid. Please choose a valid URL.");
+}
+
+function createSiteLink(url) {
+  var titleInput = $('#title-input').val();
+  // var urlInput = $('#url-input').val();
+  var siteLink = '<li><a href="'+url+'">'+titleInput+'</a><button class="remove-link">x</button><button class="mark-as-read">Mark as Read</li>'
+  debugger;
   addToLinkList(siteLink);
   totalLinks();
 }
 
-// function addToLinkList(siteLink) {
-//   $('.linked-list').append(siteLink);
-// }
-//
-// totalLinks()
-//
-// $('#title-input').on('keyup', function() {
-//   if ($('#title-input').val() !== ''){
-//     $('#add-link-button').prop('disabled', false);
-//   } else {
-//     $('#add-link-button').prop('disabled', true);
-//   }
-// })
+function addToLinkList(siteLink) {
+  $('.linked-list').append(siteLink);
+}
+
+function totalLinks() {
+  var total = 0;
+  total++;
+}
+
+$('#title-input').on('keyup', function() {
+  if ($('#title-input').val() !== ''){
+    $('#add-link-button').prop('disabled', false);
+  } else {
+    $('#add-link-button').prop('disabled', true);
+  }
+});
 // && $('#url-input').val() !== ''
