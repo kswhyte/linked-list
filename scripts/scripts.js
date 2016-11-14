404f5011593364ca1b7652ad6a2b98b29dc1f5bb
KINAN WHYTE <kinanwhyte@gmail.com>
2016-11-14 11:52:26 -0700

    adds final touches to UI

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -37,7 +37,7 @@ function invalidUrlErrorMessage() {
 
 function createSiteLink(urlInput) {
   var titleInput = $('#title-input').val();
-  var siteLink = '<li><a class="new-url-link" target="_blank" href="http://' + urlInput + '">' + titleInput + '</a><button class="mark-as-read-button">Mark as Read</button><button class="remove-link-button""mark-as-read-button">X</li>'
+  var siteLink = '<li><a class="new-url-link" target="_blank" href="http://' + urlInput + '">' + titleInput + '</a><button class="mark-as-read-button">Mark as Read</button><button class="remove-link-button""mark-as-read-button">X</li>';
   addToLinkList(siteLink);
   $('#title-input').focus();
 }
